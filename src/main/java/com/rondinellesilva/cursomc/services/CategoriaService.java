30ac9ee82f09c912cf41888a5ccedbed43241786
Rondinelle Pessoa da Silva <rondinellepessoa@gmail.com>
2019-11-08 06:52:18 +0000

    Paginacao com parametros opcionais na requisicao

--- src/main/java/com/rondinellesilva/cursomc/services/CategoriaService.java
+++ src/main/java/com/rondinellesilva/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.rondinellesilva.cursomc.domain.Categoria;
@@ -49,5 +52,10 @@ public class CategoriaService {
 
 	public List<Categoria> findAll() {
 		return repo.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPage, String direction,String orderBy){
+		PageRequest pageRequest = PageRequest.of(page, linesPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 }
