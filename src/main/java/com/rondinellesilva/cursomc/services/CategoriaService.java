5f8335e0244417907817fd7af5415708b10b9f2d
Rondinelle Pessoa da Silva <rondinellepessoa@gmail.com>
2019-11-09 07:43:56 +0000

    Correcoes: update em CategoriaService e construtor Pagamento

--- src/main/java/com/rondinellesilva/cursomc/services/CategoriaService.java
+++ src/main/java/com/rondinellesilva/cursomc/services/CategoriaService.java
@@ -38,8 +38,9 @@ public class CategoriaService {
 	}
 
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
 
 	public void delete(Long id) {
@@ -62,5 +63,9 @@ public class CategoriaService {
 	
 	public Categoria fromDTO(CategoriaDTO dto) {
 		return new Categoria(dto.getId(), dto.getNome());
+	}
+	
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
 	}
 }
