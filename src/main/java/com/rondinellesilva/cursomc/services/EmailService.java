ae5f81b6e00fbbb0cdb34a0eb5d1572559a05b0a
Rondinelle Pessoa da Silva <rondinellepessoa@gmail.com>
2019-12-10 06:37:32 +0000

    Email HTML

--- src/main/java/com/rondinellesilva/cursomc/services/EmailService.java
+++ src/main/java/com/rondinellesilva/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.rondinellesilva.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.rondinellesilva.cursomc.domain.Pedido;
@@ -7,6 +9,10 @@ import com.rondinellesilva.cursomc.domain.Pedido;
 public interface EmailService {
 
 	void sendOrderConfirmationEmail(Pedido obj);
-	
+
 	void sendEmail(SimpleMailMessage msg);
+
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+
+	void sendHtmlEmail(MimeMessage msg);
 }
